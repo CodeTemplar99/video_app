edba81a772318e96770975f37d1c850934cfa8d7
Mitchell Chiadika <udoagwac@gmail.com>
2022-02-02 14:18:15 +0100

    login page

---
+++ lib/home_screen.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset(
+            "assets/images/logo.png",
+            width: 250,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 38.0),
+            child: TextFormField(
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: 'Enter your mobile number'),
+            ),
+          ),
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Next",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50)),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
